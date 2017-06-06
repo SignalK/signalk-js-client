512ac7dd6348adb8ec1b312554f86282099cb4d8
Tim Mathews <tim@signalk.org>
2017-06-06 08:31:28 -0400

    Stub tests for connect function

---
+++ test/connect.js
@@ -0,0 +1,25 @@
+const assert = require('chai').assert;
+const expect = require('expect.js');
+const Client = require('../index');
+
+describe('Client#connect', function() {
+  it('queries server for endpoints', function() {
+    throw new Error('not implemented');
+  });
+
+  it('subscribes to none with no parameters', function() {
+    throw new Error('not implemented');
+  });
+
+  it('subscribes to none when passed "none"', function() {
+    throw new Error('not implemented');
+  });
+
+  it('subscribes to self when passed "self"', function() {
+    throw new Error('not implemented');
+  });
+
+  it('subscribes to all when passed "all"', function() {
+    throw new Error('not implemented');
+  });
+});
